9e6b3d419c3cff3e7ce0b517d44a06b7b2214924
pestrige <pestrige@gmail.com>
2020-12-07 14:39:15 +0300

    Исправил путь до маркера

--- source/js/google-map.js
+++ source/js/google-map.js
@@ -3,7 +3,7 @@
 
     const uluru = { lat: 59.939033513366084, lng: 30.32128792141334 };
     const markerPos = { lat:59.938753982836836, lng: 30.32304743616715 };
-    const markerIcon = '../img/map-pin.png';
+    const markerIcon = './../img/map-pin.png';
 
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 16,
